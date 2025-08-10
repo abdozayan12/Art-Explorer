fe23d05e66592f76a322f7cb9dead0d77e82eac9
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2025-08-10 22:33:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
